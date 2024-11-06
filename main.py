b8a56353e518b0b9290703c25718725991a4f79a
Hanthebot <56969399+Hanthebot@users.noreply.github.com>
2024-11-06 19:02:36 +0800

    V.0.1.0 Supporting file & camera

--- main.py
+++ main.py
@@ -6,35 +6,36 @@ from PIL import Image
 import re
 
 from preprocess import preprocess_img
+from postprocess import extract_11_digit_number, make_dash_separated, validate
 
 app = Flask(__name__, static_url_path='/static')
 pytesseract.pytesseract.tesseract_cmd = <YOUR_TESSERACT_DIR>
 
+def render_error(msg: str):
+    return render_template('error.html', msg = msg)
+
 @app.route('/ocr', methods=['POST'])
 def ocr():
+    """ OCR """
     if 'file' not in request.files:
-        return render_template('error.html', msg = "No file part")
+        return render_error("No file part")
 
     file = request.files['file']
     if file.filename == '':
-        return render_template('error.html', msg = "No selected file")
+        return render_error("No selected file")
 
     if file:
         image = Image.open(file.stream)
         preprocessed = preprocess_img(image)
         ocr_result = pytesseract.image_to_string(preprocessed, lang='eng')
-
-        return render_template('ocr_rst.html', ocr_result = ocr_result)
-
-    return render_template('error.html', msg = "no file")
-
-def extract_11_digit_number(text):
-    """Extracts an 11-digit number ending with 0-6 from the given text, considering possible separators."""
-    pattern = r'\b(?:\d[\s.,-]*){10}[0-6]\b'
-    match = re.search(pattern, text)
-    if match:
-        return re.sub(r'[\s.,-]', '', match.group())
-    return None
+        waybill = extract_11_digit_number(ocr_result)
+        if waybill:
+            if validate(waybill):
+                return render_template('ocr_rst.html', result = make_dash_separated(waybill))
+            return render_error(f"failed validation: {waybill}")
+        else:
+            return render_error(f"failed to detect from: {ocr_result}")
+    return render_error("no file")
 
 @app.route('/')
 def root():
